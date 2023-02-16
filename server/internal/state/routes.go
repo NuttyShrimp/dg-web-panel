f901e78ce66450dcfcabb7975770b495315325bb
NuttyShrimp <jan.lecoutere@hotmail.be>
2023-02-16 12:46:25 +0100

    feat(ci): introduce image building

---
+++ server/internal/state/routes.go
@@ -0,0 +1,37 @@
+package state
+
+import (
+	"degrens/panel/internal/auth/middlewares/role"
+	"degrens/panel/internal/routes"
+	"degrens/panel/lib/log"
+
+	"github.com/gin-gonic/gin"
+)
+
+type StateRouter struct {
+	routes.Router
+}
+
+func NewStateRouter(rg *gin.RouterGroup, logger log.Logger) {
+	router := &StateRouter{
+		routes.Router{
+			RouterGroup: rg.Group("/staff", role.New([]string{"developer"})),
+			Logger:      logger,
+		},
+	}
+	router.RegisterRoutes()
+}
+
+func (ST *StateRouter) RegisterRoutes() {
+	ST.RouterGroup.GET("/schedule/update", ST.FetchScheduledUpdate)
+	ST.RouterGroup.POST("/schedule/update", ST.ScheduleUpdate)
+}
+
+func (ST *StateRouter) ScheduleUpdate(ctx *gin.Context) {
+	scheduleUpdate()
+	ctx.JSON(200, gin.H{})
+}
+
+func (ST *StateRouter) FetchScheduledUpdate(ctx *gin.Context) {
+	ctx.JSON(200, incomingUpdate)
+}
