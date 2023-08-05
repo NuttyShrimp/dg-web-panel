94e8b0fdd255987189ecc832eae2a5612869286b
NuttyShrimp <jan.lecoutere@hotmail.be>
2023-08-05 15:19:39 +0200

    fix(penalties): fetch last ban for isBanned and skip expired ones + reverse penalty list

--- server/internal/cfx/penalties/penalties.go
+++ server/internal/cfx/penalties/penalties.go
@@ -32,7 +32,7 @@ type BanInfo struct {
 
 func GetPlayerPenalties(steamId string) ([]cfx_models.Penalties, error) {
 	penalties := []cfx_models.Penalties{}
-	err := db.CfxMariaDB.Client.Where("steamId = ?", steamId).Find(&penalties).Error
+	err := db.CfxMariaDB.Client.Where("steamId = ?", steamId).Order("date DESC").Find(&penalties).Error
 	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
 		return nil, err
 	}
@@ -48,7 +48,7 @@ func GetBanList() ([]*cfx_models.Penalties, error) {
 
 func IsPlayerBanned(steamId string) (*time.Time, error) {
 	penalty := cfx_models.Penalties{}
-	err := db.CfxMariaDB.Client.Where("steamId = ? AND penalty = ?", steamId, cfx_models.BanPenalty).First(&penalty).Error
+	err := db.CfxMariaDB.Client.Where("steamId = ? AND penalty = ?", steamId, cfx_models.BanPenalty).Order("date DESC").First(&penalty).Error
 	if err != nil {
 		if err == gorm.ErrRecordNotFound {
 			return nil, nil
@@ -59,7 +59,10 @@ func IsPlayerBanned(steamId string) (*time.Time, error) {
 		startTime := time.Unix(0, 0)
 		return &startTime, nil
 	}
-	until := penalty.Date.Add(time.Duration(penalty.Length) * time.Second)
+	until := penalty.Date.Add(time.Duration(penalty.Length) * time.Hour * 24)
+	if time.Now().After(until) {
+		return nil, nil
+	}
 	return &until, nil
 }
 
