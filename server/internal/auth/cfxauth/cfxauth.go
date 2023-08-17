cc2a01cf347bdd4d879bf7c751cb9973c0749199
NuttyShrimp <jan.lecoutere@hotmail.be>
2023-08-17 23:13:46 +0200

    fix(cfxAuth): put token in url query

--- server/internal/auth/cfxauth/cfxauth.go
+++ server/internal/auth/cfxauth/cfxauth.go
@@ -12,7 +12,9 @@ import (
 func AuthorizeToken(ctx *gin.Context, token string) error {
 	id := cfxtoken.GetNewToken()
 	tokenInfo := cfxtoken.TokenInfo{}
-	ai, err := api.CfxApi.DoRequest("GET", fmt.Sprintf("/tokens/info/%s", token), nil, &tokenInfo)
+	ai, err := api.CfxApi.DoRequest("GET", "/tokens/info", map[string]string{
+		"token": token,
+	}, &tokenInfo)
 	if err != nil {
 		return err
 	}
