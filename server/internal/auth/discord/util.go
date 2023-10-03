ee9763e7be98945f995b6bb61046891d1c18c6ee
NuttyShrimp <jan.lecoutere@hotmail.be>
2023-10-03 17:42:40 +0200

    fix(server/discord): add custom error when discord api fails

--- server/internal/auth/discord/util.go
+++ server/internal/auth/discord/util.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 
 	dgerrors "degrens/panel/lib/errors"
 
@@ -15,6 +16,9 @@ func fetchFromDiscordAPI(token *oauth2.Token, path string, target interface{}) e
 	res, err := info.Conf.Client(context.Background(), token).Get("https://discord.com/api/" + path)
 
 	if err != nil || res.StatusCode != 200 {
+		if err == nil {
+			err = fmt.Errorf("Discord API responded with status code %d: %s", res.StatusCode, res.Status)
+		}
 		logrus.WithField("statusCode", res.StatusCode).WithError(err).Error("Error while getting user info")
 		return errors.New("error while getting user info")
 	}
